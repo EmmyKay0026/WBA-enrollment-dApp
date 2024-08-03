f8af2ac4299a822b18866e404defe953fc1feba3
SnapDevTeam2 <devteam2@snapbrillia.com>
2024-08-03 07:05:12 +0100

    updated

--- transfer.ts
+++ transfer.ts
@@ -16,31 +16,6 @@ const connection = new Connection("https://api.devnet.solana.com");
 
 (async () => {
   try {
-    const transaction = new Transaction().add(
-      SystemProgram.transfer({
-        fromPubkey: from.publicKey,
-        toPubkey: to,
-        lamports: LAMPORTS_PER_SOL / 100,
-      })
-    );
-    transaction.recentBlockhash = (
-      await connection.getLatestBlockhash("confirmed")
-    ).blockhash;
-    transaction.feePayer = from.publicKey;
-
-    // Sign transaction, broadcast, and confirm
-    const signature = await sendAndConfirmTransaction(connection, transaction, [
-      from,
-    ]);
-    console.log(`Success! Check out your TX here: 
-        https://explorer.solana.com/tx/${signature}?cluster=devnet`);
-  } catch (e) {
-    console.error(`Oops, something went wrong: ${e}`);
-  }
-})();
-
-(async () => {
-  try {
     // Get balance of dev wallet
     const balance = await connection.getBalance(from.publicKey);
 
